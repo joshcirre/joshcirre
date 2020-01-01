00a6510b41ffec9021d6a78be5c98437634120cd
Josh Cirre <hello@joshcirre.com>
2020-01-01 16:49:15 -0700

    added some blog posts

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,27 @@
 import React from "react"
 import { Link } from "gatsby"
-
+import PostPreview from "../components/post-preview"
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-  </Layout>
-)
+import usePosts from "../hooks/use-posts"
+
+const IndexPage = () => {
+  const posts = usePosts()
+
+  return (
+    <Layout>
+      <SEO title="Home" />
+      <h1>Hi people</h1>
+      <p>Welcome to your new Gatsby site.</p>
+      <p>Now go build something great.</p>
+      <h2>Read my blog</h2>
+      {posts.map(post => (
+        <PostPreview key={post.slug} post={post} />
+      ))}
+    </Layout>
+  )
+}
 
 export default IndexPage
